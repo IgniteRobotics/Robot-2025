9906fa9b2067db6d20583e920deb6bc0d2c26740
Kelsey Crouch <kelseygenevacrouch@gmail.com>
2024-12-29 18:06:18 -0500

    Kelsey's first commit

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -96,7 +96,7 @@ public class Elevator implements Subsystem {
   private void mechConfigure(){
     mech = new Mechanism2d(3, 3);
     root = mech.getRoot("elevator", 2, 0);
-    arm = root.append(new MechanismLigament2d("elevator", 1, 90));
+    arm = root.append(new MechanismLigament2d("arm", 1, 90));
     wrist =
         arm.append(
             new MechanismLigament2d("wrist", 0.5, 90, 6, new Color8Bit(Color.kPurple)));
