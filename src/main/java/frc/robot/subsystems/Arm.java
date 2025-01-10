00df2be63cdfe0d633f4d913b684fc0b8bbccd03
William Crouch <richardwilliamcrouch@gmail.com>
2025-01-09 20:28:17 -0500

    Logging and configuring

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -6,11 +6,7 @@ import java.util.function.Supplier;
 
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.Utils;
-import com.ctre.phoenix6.configs.MotionMagicConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.*;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
@@ -26,6 +22,7 @@ import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -48,10 +45,15 @@ import frc.robot.Robot;
 import frc.robot.Preferences.DoublePreference;
 import frc.robot.generated.TunerConstants;
 
+@Logged
 public class Arm implements Subsystem {
-    private final TalonFX m_armMotor;
+  private final TalonFX m_armMotor;
 
   private Slot0Configs m_Slot0Configs = new Slot0Configs();
+
+  private Slot1Configs m_Slot1Configs = new Slot1Configs();
+
+  private Slot2Configs m_Slot2Configs = new Slot2Configs();
   
   private SoftwareLimitSwitchConfigs m_softLimitConfig = new SoftwareLimitSwitchConfigs();
 
@@ -80,6 +82,12 @@ public class Arm implements Subsystem {
 
     m_Slot0Configs = TunerConstants.ArmConstants.createSlot0Configs(); 
     m_armMotor.getConfigurator().apply(m_Slot0Configs);
+
+    m_Slot1Configs = TunerConstants.ArmConstants.createSlot1Configs();
+    m_armMotor.getConfigurator().apply(m_Slot1Configs);
+
+    m_Slot2Configs = TunerConstants.ArmConstants.createSlot2Configs();
+    m_armMotor.getConfigurator().apply(m_Slot2Configs);
 
     m_softLimitConfig = TunerConstants.ArmConstants.createSoftLimitConigs(); 
     m_armMotor.getConfigurator().apply(m_softLimitConfig);
