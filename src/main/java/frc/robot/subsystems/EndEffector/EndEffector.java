4dc9d60d21dbe1121e6d2ec273371a8ef8916aba
Joshua Reddick <y0shi@noreply.github.com>
2025-02-19 20:53:41 -0500

    oops

--- src/main/java/frc/robot/subsystems/EndEffector/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector/EndEffector.java
@@ -107,8 +107,7 @@ public class EndEffector extends SubsystemBase {
   public void configureCANrange(){
     ProximityParamsConfigs proximityParamsConfigs = new ProximityParamsConfigs();
     m_beambreak.getConfigurator().refresh(proximityParamsConfigs);
-    proximityParamsConfigs.withProximityThreshold(Units.Inches.of(.5));
-    m_beambreak.getConfigurator().apply(proximityParamsConfigs);
+    m_beambreak.getConfigurator().apply(proximityParamsConfigs.withProximityThreshold(Units.Inches.of(.5)));
   }
 
   public void outtakeCoral(){
