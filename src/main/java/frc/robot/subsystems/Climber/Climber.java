49b3946c65afb396b84c9f8d4786fe4424ae0b07
Kelsey Crouch <kelseygenevacrouch@gmail.com>
2025-02-20 18:25:11 -0500

    first commit

---
+++ src/main/java/frc/robot/subsystems/Climber/Climber.java
@@ -0,0 +1,146 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Climber;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.epilogue.NotLogged;
+import edu.wpi.first.epilogue.Logged.Importance;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.PreferenceTypes.DoublePreference;
+
+@Logged
+public class Climber implements Subsystem {
+    private final TalonFX m_climberMotorLeader;
+    private final TalonFX m_climberMotorFollower;
+
+  private Slot0Configs m_Slot0Configs = new Slot0Configs();
+  
+  private SoftwareLimitSwitchConfigs m_softLimitConfig = new SoftwareLimitSwitchConfigs();
+
+  private MotionMagicConfigs m_motionMagicConfigs = new MotionMagicConfigs();
+
+  private MotorOutputConfigs m_leaderMotorConfig = new MotorOutputConfigs();
+
+  private MotorOutputConfigs m_followerMotorConfig = new MotorOutputConfigs();
+
+  private TalonFXConfiguration m_fxCfg = new TalonFXConfiguration();
+
+  public MotionMagicVoltage m_MMPosition =   new MotionMagicVoltage(0);
+
+  //logged stuff
+  @Logged(name = "Target Position", importance = Importance.CRITICAL)
+  private double m_targetPosition;
+
+  /** Creates a new Climber. */
+  public Climber() {
+    m_climberMotorLeader = ClimberConstants.CLIMBER_LEADER_MOTOR; 
+    m_climberMotorFollower = ClimberConstants.CLIMBER_FOLLOWER_MOTOR;
+
+    m_Slot0Configs = ClimberConstants.createSlot0Configs(); 
+    m_climberMotorLeader.getConfigurator().apply(m_Slot0Configs);
+    m_climberMotorFollower.getConfigurator().apply(m_Slot0Configs);
+
+    m_softLimitConfig = ClimberConstants.createSoftLimitConigs(); 
+    m_climberMotorLeader.getConfigurator().apply(m_softLimitConfig);
+    m_climberMotorFollower.getConfigurator().apply(m_softLimitConfig);
+
+    m_motionMagicConfigs = ClimberConstants.createMotionMagicConfigs();
+    m_climberMotorLeader.getConfigurator().apply(m_motionMagicConfigs);
+    m_climberMotorFollower.getConfigurator().apply(m_motionMagicConfigs);
+
+    m_leaderMotorConfig = ClimberConstants.createLeaderMotorOutputConfigs();
+    m_climberMotorLeader.getConfigurator().apply(m_leaderMotorConfig);
+
+    m_followerMotorConfig = ClimberConstants.createFollowerMotorOutputConfigs();
+    m_climberMotorFollower.getConfigurator().apply(m_followerMotorConfig);
+
+  }
+
+  @NotLogged
+  public void setPositionRevolutions(double position) {
+    m_targetPosition = position;
+    m_climberMotorLeader.setControl(m_MMPosition.withPosition(position).withSlot(0));
+    m_climberMotorFollower.setControl(m_MMPosition.withPosition(position).withSlot(0));
+  }
+  
+  @NotLogged
+  public void setPositionRevolutions(DoublePreference position){
+    setPositionRevolutions(position.get());
+  }
+
+  @Logged
+  public double getPosition(){
+    return m_climberMotorLeader.getPosition().getValueAsDouble();
+  }
+
+
+  @Override
+  public void periodic() {
+    
+  }
+
+  @Override
+  public void simulationPeriodic() {
+
+  }
+
+  public void setElevatorPID(DoublePreference P, DoublePreference D, DoublePreference I, DoublePreference G){
+    m_Slot0Configs = new Slot0Configs();
+
+    m_climberMotorLeader.getConfigurator().refresh(m_Slot0Configs);
+
+    m_Slot0Configs.withKP(P.getValue()).withKD(D.getValue()).withKI(I.getValue()).withKG(G.getValue());
+
+    m_climberMotorLeader.getConfigurator().apply(m_Slot0Configs);
+    m_climberMotorFollower.getConfigurator().apply(m_Slot0Configs);
+  }
+
+  @Logged(name = "Actual kP", importance = Importance.CRITICAL)
+  public double getElevatorkP(){
+    m_Slot0Configs = new Slot0Configs();
+    m_climberMotorLeader.getConfigurator().refresh(m_Slot0Configs);
+    return m_Slot0Configs.kP;
+  }
+
+  @Logged(name = "Actual kD", importance = Importance.CRITICAL)
+  public double getElevatorkD(){
+    m_Slot0Configs = new Slot0Configs();
+    m_climberMotorLeader.getConfigurator().refresh(m_Slot0Configs);
+    return m_Slot0Configs.kD;
+  }
+
+  @Logged(name = "Actual kI", importance = Importance.CRITICAL)
+  public double getElevatorkI(){
+    m_Slot0Configs = new Slot0Configs();
+    m_climberMotorLeader.getConfigurator().refresh(m_Slot0Configs);
+    return m_Slot0Configs.kI;
+  }
+
+  @Logged(name = "Actual kG", importance = Importance.CRITICAL)
+  public double getElevatorkG(){
+    m_Slot0Configs = new Slot0Configs();
+    m_climberMotorLeader.getConfigurator().refresh(m_Slot0Configs);
+    return m_Slot0Configs.kG;
+  }
+
+  @Logged(name = "Voltage", importance = Importance.CRITICAL)
+  public double getVoltage(){
+    return m_climberMotorLeader.getMotorVoltage().getValueAsDouble();
+  }
+
+  @Logged(name = "Current", importance = Importance.CRITICAL)
+  public double getCurrent(){
+    return m_climberMotorLeader.getStatorCurrent().getValueAsDouble();
+  }
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
@@ -0,0 +1,80 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Climber;
+
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+
+public class ClimberConstants {
+
+        //Motor
+        public static final int kEclimberMotorLeaderId = 6;
+        public static final int kEclimberMotorFollowerId = 7;
+        public static final TalonFX CLIMBER_LEADER_MOTOR = new TalonFX(kEclimberMotorLeaderId);
+        public static final TalonFX CLIMBER_FOLLOWER_MOTOR = new TalonFX(kEclimberMotorFollowerId);
+
+
+        //Slot0Configs
+
+        public static final double CLIMBER_kV = 0;
+        public static final double CLIMBER_kS = 0;
+        public static final double CLIMBER_kP = 0;
+        public static final double CLIMBER_kI = 0;
+        public static final double CLIMBER_kD = 0;
+        public static final double CLIMBER_kG = 0;
+        public static final GravityTypeValue CLIMBER_GRAVITY = GravityTypeValue.Elevator_Static;
+
+        public static Slot0Configs createSlot0Configs(){ 
+            Slot0Configs slot = new Slot0Configs();
+            slot.kV = CLIMBER_kV;
+            slot.kS = CLIMBER_kS;
+            slot.kP = CLIMBER_kP;
+            slot.kI = CLIMBER_kI;
+            slot.kD = CLIMBER_kD;
+            slot.kG = CLIMBER_kG;
+            slot.GravityType = CLIMBER_GRAVITY;
+           return slot; 
+        }
+
+        //SoftLimitConfig
+        public static final double CLIMBER_FORWARD_SOFT_LIMIT = 100;
+        public static final double CLIMBER_REVERSE_SOFT_LIMIT = 0;
+
+        public static SoftwareLimitSwitchConfigs createSoftLimitConigs(){
+            SoftwareLimitSwitchConfigs newConfigs = new SoftwareLimitSwitchConfigs();
+            newConfigs.ForwardSoftLimitEnable = false;
+            newConfigs.ReverseSoftLimitEnable = false;
+            newConfigs.ForwardSoftLimitThreshold = CLIMBER_FORWARD_SOFT_LIMIT;
+             newConfigs.ReverseSoftLimitThreshold = CLIMBER_REVERSE_SOFT_LIMIT;
+                return newConfigs;
+        }
+
+        //MotionMagicConfigs
+        public static MotionMagicConfigs createMotionMagicConfigs(){
+            MotionMagicConfigs newConfigs = new MotionMagicConfigs();
+            //TODO: ADD MORE IF NECESSARY
+            return newConfigs;
+        }
+
+        //MotorConfigs
+        public static MotorOutputConfigs createLeaderMotorOutputConfigs(){
+            MotorOutputConfigs newConfigs = new MotorOutputConfigs();
+            newConfigs.Inverted = InvertedValue.Clockwise_Positive;
+            return newConfigs;
+        }
+
+        public static MotorOutputConfigs createFollowerMotorOutputConfigs(){
+            MotorOutputConfigs newConfigs = new MotorOutputConfigs();
+            newConfigs.Inverted = InvertedValue.CounterClockwise_Positive;
+            return newConfigs;
+        }
+
+
+}
