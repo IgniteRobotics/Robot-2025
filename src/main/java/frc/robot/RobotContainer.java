7a263c1300e7cd1921c0bdd40390eff6facec813
Aarav <aaravgaudana6@gmail.com>
2025-03-01 09:54:21 -0500

    reversed left joystick x and y

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -132,8 +132,8 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * m_RobotState.getMaxSpeed()) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * m_RobotState.getMaxSpeed()) // Drive left with negative X (left)
+                drive.withVelocityX(joystick.getLeftY() * m_RobotState.getMaxSpeed()) // Drive forward with negative Y (forward)
+                    .withVelocityY(joystick.getLeftX() * m_RobotState.getMaxSpeed()) // Drive left with negative X (left)
                     .withRotationalRate(-joystick.getRightX() * m_RobotState.getMaxRotation()) // Drive counterclockwise with negative X (left)
             )
         );
