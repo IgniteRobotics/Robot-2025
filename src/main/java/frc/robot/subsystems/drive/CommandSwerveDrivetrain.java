e7d1a31487387eb37fb5f557d612bf6e46763205
William Crouch <richardwilliamcrouch@gmail.com>
2025-03-01 10:43:16 -0500

    Scoping is fun

--- src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
@@ -283,12 +283,12 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
 
     public Command sysIdTranslation(){
         return m_sysIdRoutineTranslation.quasistatic(Direction.kForward).withTimeout(6).andThen(m_sysIdRoutineTranslation.quasistatic(Direction.kReverse).withTimeout(6))
-            .andThen(m_sysIdRoutineTranslation.dynamic(Direction.kForward)).withTimeout(4).andThen(m_sysIdRoutineTranslation.dynamic(Direction.kReverse)).withTimeout(4);
+            .andThen(m_sysIdRoutineTranslation.dynamic(Direction.kForward).withTimeout(4)).andThen(m_sysIdRoutineTranslation.dynamic(Direction.kReverse).withTimeout(4));
     }
 
     public Command sysIdRotation(){
         return m_sysIdRoutineRotation.quasistatic(Direction.kForward).withTimeout(6).andThen(m_sysIdRoutineRotation.quasistatic(Direction.kReverse).withTimeout(6))
-            .andThen(m_sysIdRoutineRotation.dynamic(Direction.kForward)).withTimeout(4).andThen(m_sysIdRoutineRotation.dynamic(Direction.kReverse)).withTimeout(4);
+            .andThen(m_sysIdRoutineRotation.dynamic(Direction.kForward).withTimeout(4)).andThen(m_sysIdRoutineRotation.dynamic(Direction.kReverse).withTimeout(4));
     }
 
 
@@ -425,28 +425,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     // @Override
     // public void addVisionMeasurement(Pose2d visionRobotPoseMeters, double timestampSeconds) {
     //     super.addVisionMeasurement(visionRobotPoseMeters, Utils.fpgaToCurrentTime(timestampSeconds));
-    // }
-
-    /**
-     * Adds a vision measurement to the Kalman Filter. This will correct the odometry pose estimate
-     * while still accounting for measurement noise.
-     * <p>
-     * Note that the vision measurement standard deviations passed into this method
-     * will continue to apply to future measurements until a subsequent call to
-     * {@link #setVisionMeasurementStdDevs(Matrix)} or this method.
-     *
-     * @param visionRobotPoseMeters The pose of the robot as measured by the vision camera.
-     * @param timestampSeconds The timestamp of the vision measurement in seconds.
-     * @param visionMeasurementStdDevs Standard deviations of the vision pose measurement
-     *     in the form [x, y, theta]ᵀ, with units in meters and radians.
-     */
-    // @Override
-    // public void addVisionMeasurement(
-    //     Pose2d visionRobotPoseMeters,
-    //     double timestampSeconds,
-    //     Matrix<N3, N1> visionMeasurementStdDevs
-    // ) {
-    //     super.addVisionMeasurement(visionRobotPoseMeters, Utils.fpgaToCurrentTime(timestampSeconds), visionMeasurementStdDevs);
     // }
 
     public double getYaw(){
