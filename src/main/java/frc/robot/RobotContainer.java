38c7483ccbd58d43bea8f1baf06ffd83667fb8cf
William Crouch <richardwilliamcrouch@gmail.com>
2025-03-07 12:42:21 -0500

    Panic button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,6 +28,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
@@ -276,6 +277,13 @@ public class RobotContainer {
             new ToSetpoint(elevator, ElevatorConstants.FLOOR.GROUND.position).alongWith(
                 new InstantCommand(() -> collector.setWristPosition(AlgaeCollectorConstants.WRIST.STOW.angle))).alongWith(
                 new InstantCommand(() -> collector.stopAlgaeMotor()))
+        );
+
+        joystick.back().onTrue(new InstantCommand(() -> CommandScheduler.getInstance().cancelAll())
+        .andThen(new InstantCommand(() -> collector.stopAlgaeMotor()))
+        .andThen(new InstantCommand(() -> corraler.stopCoralMotor()))
+        .andThen(new InstantCommand(() -> elevator.setPositionRevolutions(ElevatorConstants.FLOOR.GROUND.position)))
+        .andThen(new InstantCommand(() -> collector.stow()))
         );
 
                                          
