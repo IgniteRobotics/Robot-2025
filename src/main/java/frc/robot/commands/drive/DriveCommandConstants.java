2e13ad6741b20f7ec7d889d28016b22d094c080e
William Crouch <richardwilliamcrouch@gmail.com>
2025-04-03 09:35:55 -0400

    Made constants up to date

--- src/main/java/frc/robot/commands/drive/DriveCommandConstants.java
+++ src/main/java/frc/robot/commands/drive/DriveCommandConstants.java
@@ -11,17 +11,17 @@ import static edu.wpi.first.units.Units.*;
 public class DriveCommandConstants {
 
     public static final double ROTATION_P = 0.1;
-    public static final double ROTATION_D = 0.001;
+    public static final double ROTATION_D = 0.00005;
 
     public static final double DRIVE_X_P = 2.6;
-    public static final double DRIVE_X_D = 0.1;
+    public static final double DRIVE_X_D = 0.015;
 
-    public static final double DRIVE_Y_P = 0.2;
-    public static final double DRIVE_Y_D = 0.01;
+    public static final double DRIVE_Y_P = 2.6;
+    public static final double DRIVE_Y_D = 0.000001;
 
-    public static final double DRIVE_Y_TOLERANCE = 0.1;
+    public static final double DRIVE_Y_TOLERANCE = 0.25;
     public static final double DRIVE_X_TOLERANCE = 0.1;
-    public static final double ROTATION_TOLERANCE = 0.1;
+    public static final double ROTATION_TOLERANCE = 3.5;
 
     public static final double REEF_PUSH_DRIVE_X = 0.75;
 
